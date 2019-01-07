6fc7d5b942b7c035b2bdd45eb9dd3058b1a2e48e
jingzhix <katherainxie@gmail.com>
2019-01-07 15:47:58 +0800

    1.03% of C++ online submissions...A lot of patchwork

--- 010.cpp
+++ 010.cpp
@@ -39,11 +39,60 @@ public:
 		//Case B: X*
 		}else{
 			if(p[pstart]=='.'){
-				for(int i=0;i<=slen-sstart;i++)
-					if(isMatch(s, p, sstart+i, pstart+2))
-						return true;
-				return false;
+				//Case B1: X*abc: find the location of a
+				if((pstart+3==plen&&p[pstart+2]!='.')||(pstart+3<plen&&p[pstart+3]!='*'&&p[pstart+2]!='.')){
+					while(sstart<slen){
+						if(s[sstart]==p[pstart+2]&&isMatch(s, p, sstart+1, pstart+3))
+							return true;
+						sstart++;
+					}
+					return false;
+				}
+				//Case B2: X*.: 
+				else if((pstart+3==plen&&p[pstart+2]=='.')||(pstart+3<plen&&p[pstart+3]!='*'&&p[pstart+2]=='.')){
+					for(int i=1;i<=slen-sstart;i++)
+						if(isMatch(s, p, sstart+i, pstart+3))
+							return true;
+					return false;
+				}
+				//Case B3: X*X*: Equal or not Equal
+				else if(pstart+3<plen&&p[pstart+3]=='*'){
+				   if(p[pstart+2]==p[pstart])	   
+					   return isMatch(s,p, sstart, pstart+2);
+				   else{
+						for(int i=0;i<=slen-sstart;i++)
+							if(isMatch(s, p, sstart+i, pstart+2))
+								return true;
+						return false;
+				   }
+				}else{
+					return true;
+				}
 			}else{
+				if((pstart+3==plen&&p[pstart+2]!='.')||(pstart+3<plen&&p[pstart+3]!='*'&&p[pstart+2]!='.')){
+					if(p[pstart+2]!=p[pstart]){
+						while(s[sstart]!=p[pstart+2]){
+							if(s[sstart]!=p[pstart])
+								return false;
+							sstart++;
+							if(sstart>=slen)
+								return false;
+						}
+						return isMatch(s, p, sstart+1, pstart+3);
+					}else{
+						if(s[sstart]!=p[pstart])
+							return false;
+						else{
+							int i=1;
+							do{
+								if(isMatch(s, p, sstart+i, pstart+2))
+									return true;
+								i++;
+							}while(s[sstart+i-1]==p[pstart]);
+							return isMatch(s, p, sstart, pstart+2);
+						}
+					}
+				}
 				int i=0;
 				do{
 					if(isMatch(s, p, sstart+i, pstart+2))
@@ -53,7 +102,6 @@ public:
 				return isMatch(s, p, sstart, pstart+2);
 			}
     	}
-
 	}
 };
 
