61903036de97998da8b1c68e45b05c0927a886c2
jingzhix <katherainxie@gmail.com>
2019-01-09 22:18:17 +0800

    LET ME SLEEP..faster than 17.47% of C++ online submissions

---
+++ 027.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<string>
+
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+		int cnt = 0;
+		for(int i=0; i<nums.size(); i++){
+			if(nums[i]==val){
+				nums.erase(nums.begin()+i);
+				i--;
+			}
+		}
+		return nums.size();
+    }
+};
+
+int main(){
+	Solution s;
+	std::string line;
+	while(std::getline(std::cin,line)){
+		int val, data;
+		std::vector<int> v;
+		std::istringstream is(line);
+		std::cin>>val;
+		while(is>>data){
+			v.push_back(data);
+			if(is.peek()==',')
+				is.ignore();
+		}
+		std::cout<<s.removeElement(v, val)<<std::endl;
+		for(auto i : v)
+			std::cout<<i<<" ";
+		std::cout<<std::endl;
+	}
+}
