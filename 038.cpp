fc286750046a6716f2de979fe535c9e5457ab621
jingzhix <katherainxie@gmail.com>
2019-02-03 23:45:31 +0800

    try

--- 038.cpp
+++ 038.cpp
@@ -7,6 +7,25 @@ using namespace std;
 class Solution {
 public:
     string countAndSay(int n) {
+      switch(n){
+      case 1:
+	return "1";
+	break;
+      case 2:
+	return "11";
+	break;
+      case 3:
+	return "21";
+        break;
+      case 4:
+	return "1221";
+	break;
+      case 5:
+	return "111221";
+      default:
+	return countAndSay(n-5)+countAndSay(5);
+	break;
+      }
     }
 };
 
@@ -17,6 +36,6 @@ int main(){
 		int n;
 		istringstream is(line);
 		is>>n;
-		cout<<coutAndSay(n)<<endl;
+		cout<<s.countAndSay(n)<<endl;
 	}
 }
