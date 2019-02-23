83ae03f095e4899d147b234b093886b7932a1caa
jingzhix <katherainxie@gmail.com>
2019-02-23 19:54:59 +0800

    faster than 99.83% of C++ online submissions for Remove Duplicates from Sorted List.

---
+++ 083.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+		if(head == NULL || head->next == NULL) return head;
+		ListNode* p = head;
+
+		ListNode* last = NULL;
+
+		while(p->next){
+			if(p->val == p->next->val){
+				if(!last) last = p;
+				p = p->next;
+			}else{
+				if(last) {
+					last->next = p->next;
+					p->next = NULL;
+					p = last;
+					last = NULL;
+				}else p = p->next;
+			}
+		}
+		if(last) last->next = NULL;
+		return head;
+
+    }
+};
+
+int main(){
+	Solution s;
+	string line;
+	while(getline(cin, line)){
+		replace(line.begin(), line.end(), '-', ' ');
+		replace(line.begin(), line.end(), '>', ' ');
+
+		istringstream is(line);
+
+		ListNode dummy(0);
+		ListNode* p = &dummy;
+		int n;
+		while(is >> n){
+			p->next = new ListNode(n);
+			p = p->next;
+		}
+
+		dummy.next = s.deleteDuplicates(dummy.next);
+		p = dummy.next;
+		while(p!=NULL){
+			cout<<p->val<<" ";
+			p = p -> next;
+		}
+	}
+}
