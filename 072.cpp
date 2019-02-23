3509b95c0368835c9c59e69bf77f768fca586cda
jingzhix <katherainxie@gmail.com>
2019-02-23 19:09:50 +0800

    faster than 100.00% of C++ online submissions for Search in Rotated Sorted Array II.

--- 072.cpp
+++ 072.cpp
@@ -7,8 +7,8 @@ using namespace std;
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-		int len1 = word2.length();
-		int len2 = word1.length();
+		int len1 = word1.length();
+		int len2 = word2.length();
 		int d[len1+1][len2+1];
 		memset(d[0], 0, sizeof(int)*(len1+1)*(len2+1));
 
@@ -23,30 +23,57 @@ public:
 		int ind1 = len1-1;
 		int ind2 = len2-1;
 
-		int deletes = 0;
-		int inserts = 0;
-		int modifis = 0;
-
-		while(ind1 > 0 && ind2 > 0){
-			if(word2[ind1] != word1[ind2]){
-				if(d[ind1+1][ind2] >= d[ind1][ind2+1]){
-					deletes++;
+		//Sign : last operation is "D", "I", "M", "R"
+		vector<char> op;
+		while(ind1 >=  0 && ind2 >= 0){
+			if(word1[ind1] != word2[ind2]){
+				if(d[ind1+1][ind2] > d[ind1][ind2+1]){
+					if(op.size() > 0 && op.back() == 'D'){
+						op.pop_back();
+						op.push_back('M');
+					}else op.push_back('I');
 					--ind2;
-				}else{
-					inserts++;
+				}else if(d[ind1+1][ind2] < d[ind1][ind2+1]){
+					if(op.size() > 0 && op.back() == 'I'){
+						op.pop_back();
+						op.push_back('M');
+					}else op.push_back('D');
 					--ind1;
+				}else{
+					if(ind1 > ind2){
+						--ind1;
+						op.push_back('D');
+					}else{
+						--ind2;
+						op.push_back('I');
+					}
 				}
 			}else{
 				--ind1;
 				--ind2;
+				op.push_back('R');
 			}
 		}
 
-		if(word2[ind1] == word1[ind2]) inserts += ind2 + ind1;
-		else if(ind1 == ind2) modifis++;
-		else inserts += ind2 + ind1;
+		while(ind1-- >= 0) op.push_back('D');
+		while(ind2-- >= 0) op.push_back('I');
+		for(auto i : op)
+			cout<<i<<" ";
+		cout<<endl;
 
-		return deletes + inserts + modifis;
+		int ret = 0;
+		int dd = 0;
+		int ii = 0;
+		int rr = 0;
+		while(i >= 0){
+			char c = op[i];
+			if(c == 'D') ++dd;
+			else if(c == 'I') ++ii;
+			else if(c == 'R' && ((!dd && !ii)||(op.))) ++rr;
+		}
+
+
+		return 8;
     }
 };
 
--- 081.cpp
+++ 081.cpp
@@ -7,10 +7,72 @@ using namespace std;
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
+        int size = nums.size();
+        if(size == 0) return false;
+        if(size == 1) return nums[0] == target;
+        
+        int point = -1;
+        int l = 0, r = size;
+        while(l < r){
+            int mid = (l+r)/2;
+            
+            if(mid == size - 1 && nums[mid] < nums[mid-1]){
+                point = mid - 1;
+                break;
+            }else if(mid == size - 1){
+                point = -1;
+                break;
+            }if(nums[mid] > nums[mid+1]){
+                point = mid;
+                break;
+            }else if(nums[mid] > nums[0]){
+                l = mid + 1;
+            }else if(nums[mid] < nums[0]){
+                r = mid;
+            }else if(nums[mid] == nums[0]){
+                if(mid == 0){
+                    point = -1;
+                    break;
+                }else if(nums[mid] > nums[size-1]){
+                    l = mid + 1;
+                }else{
+                    bool isFirst = false;
+                    for(int i = mid+1; i < size; i++)
+                        if(nums[i] > nums[0])
+                            isFirst = true;
+                    if(isFirst) l = mid + 1;
+                    else r = mid;
+                }
+            }
+        }
+        
+        if(point >= 0 && point < size-1){
+            if(target > nums[point] || target < nums[point+1]) return false;
+            if(target > nums[0]){
+                l = 0;
+                r = point + 1;
+            }else if(target < nums[0]){
+                l = point + 1;
+                r = size;
+            }else return true;
+        }else{
+            l = 0;
+            r = size;
+        }
+        
+        while(l < r){
+            int mid = (r+l)/2;
+            
+            if(nums[mid] < target)
+                l = mid + 1;
+            else if(nums[mid] > target)
+                r = mid;
+            else return true;
+        }
+        return false;
         
     }
 };
-
 int main(){
 	Solution s;
 	string line;
@@ -27,6 +89,6 @@ int main(){
 		getline(cin, line);
 		int target = stoi(line);
 
-		cout<<s.search(v, target);
+		cout<<s.search(v, target)<<endl;
 	}
 }
