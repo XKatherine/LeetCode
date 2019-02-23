93dc9799a77d865ee4a4cad6a15a0bf4a22d488a
jingzhix <katherainxie@gmail.com>
2019-02-23 20:28:24 +0800

    faster than 100.00% of C++ online submissions for Edit Distance.

--- 072.cpp
+++ 072.cpp
@@ -10,71 +10,20 @@ public:
 		int len1 = word1.length();
 		int len2 = word2.length();
 		int d[len1+1][len2+1];
-		memset(d[0], 0, sizeof(int)*(len1+1)*(len2+1));
 
-		for(int i = 0; i < len1; ++i)
-			for(int j = 0; j < len2; ++j){
-				if(word1[i] == word2[j])
-					d[i+1][j+1] = d[i][j]+1;
+		for(int i = 0; i <= len1; ++i) d[i][0] = i;
+		for(int i = 1; i <= len2; ++i) d[0][i] = i;
+
+		for(int i = 1; i <= len1; ++i)
+			for(int j = 1; j <= len2; ++j){
+				if(word1[i-1] == word2[j-1])
+					d[i][j] = min(min(d[i-1][j],d[i][j-1]) + 1,d[i-1][j-1]);
 				else
-					d[i+1][j+1] = max(d[i][j+1], d[i+1][j]);
+					d[i][j] = min(min(d[i-1][j],d[i][j-1]),d[i-1][j-1])+ 1;
 			}
 
-		int ind1 = len1-1;
-		int ind2 = len2-1;
-
-		//Sign : last operation is "D", "I", "M", "R"
-		vector<char> op;
-		while(ind1 >=  0 && ind2 >= 0){
-			if(word1[ind1] != word2[ind2]){
-				if(d[ind1+1][ind2] > d[ind1][ind2+1]){
-					if(op.size() > 0 && op.back() == 'D'){
-						op.pop_back();
-						op.push_back('M');
-					}else op.push_back('I');
-					--ind2;
-				}else if(d[ind1+1][ind2] < d[ind1][ind2+1]){
-					if(op.size() > 0 && op.back() == 'I'){
-						op.pop_back();
-						op.push_back('M');
-					}else op.push_back('D');
-					--ind1;
-				}else{
-					if(ind1 > ind2){
-						--ind1;
-						op.push_back('D');
-					}else{
-						--ind2;
-						op.push_back('I');
-					}
-				}
-			}else{
-				--ind1;
-				--ind2;
-				op.push_back('R');
-			}
-		}
-
-		while(ind1-- >= 0) op.push_back('D');
-		while(ind2-- >= 0) op.push_back('I');
-		for(auto i : op)
-			cout<<i<<" ";
-		cout<<endl;
-
-		int ret = 0;
-		int dd = 0;
-		int ii = 0;
-		int rr = 0;
-		while(i >= 0){
-			char c = op[i];
-			if(c == 'D') ++dd;
-			else if(c == 'I') ++ii;
-			else if(c == 'R' && ((!dd && !ii)||(op.))) ++rr;
-		}
-
-
-		return 8;
-    }
+		return d[len1][len2];
+	}
 };
 
 int main(){
