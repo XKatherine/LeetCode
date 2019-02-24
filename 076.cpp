3f202fd59b28bb606f4f9eccc3bb460ad48d9c47
jingzhix <katherainxie@gmail.com>
2019-02-24 09:57:53 +0800

    faster than 10.81% of C++ online submissions for Minimum Window Substring.

--- 076.cpp
+++ 076.cpp
@@ -2,12 +2,67 @@
 #include<sstream>
 #include<string>
 #include<vector>
-#include<map>
+#include<unordered_map>
+#include<deque>
 using namespace std;
+
+struct ListNode{
+	char val;
+	int index;
+	ListNode* next;
+
+	ListNode(char v, int i):val(v), index(i), next(NULL){}
+};
 
 class Solution {
 public:
     string minWindow(string s, string t) {
+		if(s == "" || t == "") return "";
+		if(s.length() < t.length()) return "";
+
+		int size = t.length();
+		unordered_map<char, int> m;
+		for(auto i : t) m[i] = 1;
+		for(auto i : t) m[i]++;
+
+		ListNode dummy(' ', 0);
+		ListNode* p = &dummy;
+
+		int start = -1;
+		int end = s.length();
+		int n = 0;
+
+		for(int i = 0; i < s.length(); i++){
+			if(m[s[i]] > 1){
+				p->next = new ListNode(s[i], i);
+				p = p->next;
+				m[s[i]] --;
+				if(++n == size && p->index - dummy.next->index < end - start){
+					start = dummy.next->index;
+					end = p->index;
+				}
+			}else if(m[s[i]] == 1){
+				ListNode* prev = &dummy;
+				while(prev->next->val != s[i]){
+					prev = prev->next;
+				}
+				if(prev->next->next){
+					prev->next = prev->next->next;
+					p->next = new ListNode(s[i], i);
+					p = p->next;
+				}else{
+					prev->next = new ListNode(s[i], i);
+					p = prev->next;
+				}
+				if(n == size && p->index - dummy.next->index < end - start){
+					start = dummy.next->index;
+					end = p->index;
+				}
+			}
+		}
+
+		if(start == -1) return "";
+		return s.substr(start, end-start+1);
     }
 };
 
